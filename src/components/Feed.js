3b25842478366ca35fae9923c022ecb175cf28b2
Fernanda Guzman <fda.guzmann@gmail.com>
2023-01-23 02:30:06 -0400

    estilos de posts y publicaciones

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -3,8 +3,6 @@ import { onNavigate } from '../main.js';
 import { firebaseAuth, getDoc, getOnDatas, getPost, updatePosts, erasePost } from '../app/firebase.js';
 import { saveTask } from '../app/getDoc.js';
 
-
-
 export const Feed = () => {
     const FeedDiv = document.createElement('div');
     const template = `
@@ -29,14 +27,11 @@ export const Feed = () => {
             </div>
         </nav>
     </section>
-    <section class="create-post" id="createPostBtn">
-        <button class="create-post-button" id="createPostButton">CREAR PUBLICACIÓN</button>
-    </section>
     <section class="posts" id="posts">
         <form class="task-form" id="taskForm">
-            <label for="user">Usuario</label>
-            <textarea class="posts-div-p" id="postsTextArea" rows="3" placeholder="PUBLICACIÓN"></textarea>
-            <button class="create-post-btn" id="createPostBtn">Publicar</button>
+            <label class="user-name" for="user">Usuario</label>
+            <textarea class="posts-div-p" id="postsTextArea" rows="3" placeholder="¿Qué piensas?"></textarea>
+            <button class="create-post-btn" id="createPostBtn">PUBLICAR</button>
         </form>
     </section>
     <div id="postsContainer">
@@ -49,93 +44,87 @@ export const Feed = () => {
     let stateEdit = false
     let id = ''
 
-    hamburger.addEventListener('click', ()=>{
+    hamburger.addEventListener('click', () => {
         // añade clases de css existan o no
         hamburger.classList.toggle('active')
         navMenu.classList.toggle('active')
     })
-    FeedDiv.querySelector('#navLinkFeed').addEventListener('click', ()=>{
+    FeedDiv.querySelector('#navLinkFeed').addEventListener('click', () => {
         hamburger.classList.remove('active')
         navMenu.classList.remove('active')
     })
-    FeedDiv.querySelector('#navLinkProfile').addEventListener('click', ()=>{
+    FeedDiv.querySelector('#navLinkProfile').addEventListener('click', () => {
         hamburger.classList.remove('active')
         navMenu.classList.remove('active')
     })
-    FeedDiv.querySelector('#navLinkCloseSession').addEventListener('click', ()=>{
+    FeedDiv.querySelector('#navLinkCloseSession').addEventListener('click', () => {
         hamburger.classList.remove('active')
         navMenu.classList.remove('active')
         signOutFun(firebaseAuth)
         onNavigate('/')
     })
-    
+
     const taskForm = FeedDiv.querySelector('.task-form')
-    const posts= FeedDiv.querySelector('.posts-div-p')
-    taskForm.addEventListener ('submit', (e) => {
-       e.preventDefault();
-       if (!stateEdit){
-        saveTask(posts.value)
-        console.log('Esto es publicar')
-       }else {
-        updatePosts(id,{description:posts.value})
-        stateEdit = false
-        id = ''
-        console.log('esto es editar')}
-       
-       taskForm.reset();
+    const posts = FeedDiv.querySelector('.posts-div-p')
+    taskForm.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (!stateEdit) {
+            saveTask(posts.value)
+            console.log('Esto es publicar')
+        } else {
+            updatePosts(id, { description: posts.value })
+            stateEdit = false
+            id = ''
+            console.log('esto es editar')
+        }
+
+        taskForm.reset();
     })
-    
+
     getOnDatas((listasPosts) => {
-        postsContainer.innerHTML =''
-        listasPosts.forEach((postsContent)=>{
-        const lista = postsContent.data();
+        postsContainer.innerHTML = ''
+        listasPosts.forEach((postsContent) => {
+            const lista = postsContent.data();
             postsContainer.innerHTML += `
-            
-        <section class="btn-posts" id="btnPosts">        
-        <div class="posts-div-btns" id="postsDivBtns">            
-            <button class="paw-posts-div-btns"><img src="../Assets/pata-blanca.png"  alt="white_paw" class="paw-img" id="pawPostsDivBtns" ></button>
-            <button class="edit-posts-div-btns" id="editPostsDivBtns" data-id="${postsContent.id}">Editar</button>
-            <button class="delete-posts-div-btns" id="deletePostsDivBtns" data-id="${postsContent.id}"></button>
-            
-        </div>
-        <section class="posts" id="posts"><div>
-        <h3>${lista.description}</h3>
-        </div></section></section>
+        
+            <section class="posts" id="posts">
+            <div class="posts-publication">
+            <h3>${lista.description}</h3>
+            </div></section>
+            <section class="btn-posts" id="btnPosts">        
+            <div class="posts-div-btns" id="postsDivBtns">            
+            <button class="paw-posts-div-btns"><img src="../Assets/patita-like.png" alt="white_paw" class="paw-img" id="pawPostsDivBtns" ></button>
+            <button class="edit-posts-div-btns"><img src="../Assets/edit-img.png" alt="edit-icon" class="edit-img" id="editPostsDivBtns" data-id="${postsContent.id}"></button>
+            <button class="delete-posts-div-btns"><img src="../Assets/delete-trash.png" alt="delete-trash" class="delete-img" id="deletePostsDivBtns" data-id="${postsContent.id}"></button>
+            </div>
+            </section>
             `
-            console.log("bandera123015",postsContent.id)
+
+            // console.log("bandera123015", postsContent.id)
         })
-    const btnEditDiv = FeedDiv.querySelectorAll(".edit-posts-div-btns")
-    const forTextArea = FeedDiv.querySelector("#postsTextArea")
-    btnEditDiv.forEach((btn) => {
-        btn.addEventListener('click', async (e) => {
-            try{
-           const getId = await getPost(e.target.dataset.id)
-           const post = getId.data() 
-           stateEdit = true
-           id = getId.id
-           forTextArea.value = post.description           
-            }catch (error){console.log("quiero llorar",error)}
-        });
-    })
+
+        const btnEditDiv = FeedDiv.querySelectorAll(".edit-posts-div-btns")
+        const forTextArea = FeedDiv.querySelector("#postsTextArea")
+        btnEditDiv.forEach((btn) => {
+            btn.addEventListener('click', async (e) => {
+                try {
+                    const getId = await getPost(e.target.dataset.id)
+                    const post = getId.data()
+                    stateEdit = true
+                    id = getId.id
+                    forTextArea.value = post.description
+                } catch (error) { console.log("quiero llorar", error) }
+            });
+        })
+
         const eraseBTn = FeedDiv.querySelectorAll(".delete-posts-div-btns");
         eraseBTn.forEach((btn) => {
-            btn.addEventListener('click', ({ target:{dataset}}) => {
-            erasePost(dataset.id)
-               
-                console.log("quiero llorar por el boton de eliminar", )
-
-        })
-
-    
-
-    });
-       
-        
+            btn.addEventListener('click', ({ target: { dataset } }) => {
+                erasePost(dataset.id)
+                console.log("quiero llorar por el boton de eliminar",)
+            })
+        });
     })
 
-    
-   
-    
-    
-       return FeedDiv;
+    return FeedDiv;
 }
